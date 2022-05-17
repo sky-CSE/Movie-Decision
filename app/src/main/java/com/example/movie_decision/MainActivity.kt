25bf86f78e75f4ca6a9bdd0ad75fba9f6ea8c843
Sumeet Yadav <ysumeet35@gmail.com>
2022-05-17 14:15:42 +0530

    Lazy Column Added

--- app/src/main/java/com/example/movie_decision/MainActivity.kt
+++ app/src/main/java/com/example/movie_decision/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.movie_decision
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.movie_decision.ui.theme.MovieDecisionTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +24,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MovieDecisionTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                MyApp {
+                    MainContent()
                 }
             }
         }
@@ -30,14 +33,73 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    MovieDecisionTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    backgroundColor = Color.Magenta,
+                    elevation = 6.dp
+                ) {
+                    Text(text = "Movies")
+                }
+            },
+        ) {
+            content() //calling content i.e. MainContent (as passed above)
+        }
+    }
+}
+
+@Composable
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Avatar",
+        "300",
+        "Harry Potter",
+        "Happiness...",
+        "Cross the Line...",
+        "Be Happy...",
+        "Happy Feet...",
+        "Life"
+    )
+) {
+    LazyColumn {
+        items(items = movieList) {
+            MovieRow(movie = it)
+        }
+    }
+
+}
+
+@Composable
+fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = 6.dp
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Icon(
+                imageVector = Icons.Default.AccountBox,
+                contentDescription = "Movie Image",
+                modifier = Modifier.size(100.dp)
+            )
+            Text(text = movie)
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MovieDecisionTheme {
-        Greeting("Android")
+        MainContent()
     }
 }
